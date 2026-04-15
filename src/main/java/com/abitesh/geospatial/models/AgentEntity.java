b34930f5eb54d0184bb429c975ba0b212d39c37e
Abitesh <221978850+Abitesh@users.noreply.github.com>
2026-04-15 22:10:51 +0530

    feat: configure PostgreSQL connection and Agent entity

---
+++ src/main/java/com/abitesh/geospatial/models/AgentEntity.java
@@ -0,0 +1,42 @@
+package com.abitesh.geospatial.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "agents")
+public class AgentEntity {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String name;
+
+    public AgentEntity() {
+    }
+
+    public AgentEntity(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
