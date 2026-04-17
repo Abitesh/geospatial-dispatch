55328dc0e406e0cc43408cda08e9a91c34b2e7bc
Abitesh <221978850+Abitesh@users.noreply.github.com>
2026-04-17 22:49:57 +0530

    second change in code

--- src/main/java/com/abitesh/geospatial/controllers/RequestController.java
+++ src/main/java/com/abitesh/geospatial/controllers/RequestController.java
@@ -1,5 +1,5 @@
 package com.abitesh.geospatial.controllers;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.abitesh.geospatial.dto.RideCreateRequest;
 import com.abitesh.geospatial.dto.RideResponse;
 import com.abitesh.geospatial.services.RequestService;
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/rides")
+@CrossOrigin(origins = "*")   // <-- add this line
 public class RequestController {
 
     private final RequestService requestService;
